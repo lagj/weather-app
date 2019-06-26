66f4142e08283e9a3768e2570d2fbcbd7a64e6ea
Luis Gamarra Jimenez <luis.gamarra26@gmail.com>
2019-06-26 15:12:42 -0700

    First project commit. Finished the storyboard layout

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Gamarra Jimenez, Luis A. on 6/26/19.
+//  Copyright © 2019 Gamarra Jimenez, Luis A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var temperatureMetric: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
